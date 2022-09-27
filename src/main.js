16fe30bc93784fa0ade23dfc97c3e32e9f9797fb
jpoliveiradev <info1joaopaulo@example.com>
2022-09-27 15:59:24 -0300

    Added Login, Finalized DashBoards and CRUDS

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VueSweetalert2 from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
@@ -18,7 +17,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
